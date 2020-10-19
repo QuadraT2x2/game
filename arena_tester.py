5b4391772ba875e03fa01c436b8cdcb4f8a76201
Kotov Sergey <serjok170394@yandex.ru>
2020-10-19 17:49:17 +0300

    3 heroes, 5 monsters, 25 prop_monsers

--- arena_tester.py
+++ arena_tester.py
@@ -1,5 +1,8 @@
 from characters import Character, Hero
-from monster_creator import Property
+from monster_creator import Property, Monster_creater
+import numpy as np
+
+
 properties = [
     Property('survivalist', 140, 0),
     Property('strong', 0, 25),
@@ -18,8 +21,16 @@ characters = [
 
 heroes = [
     Hero('Alexa', 400, 35, 'agility', 3, 5, 2),
-    Hero('Max', 650, 30, 'strength', 6, 3, 1)
+    Hero('Max', 650, 30, 'strength', 6, 3, 1),
+    Hero('Furion', 350, 25, 'intelligence', 2, 4, 7)
 ]
 
 #thief = Hero('Alexa', 400, 35, 'agility', 3, 5, 2)
-#defender = Hero('Max', 650, 30, 'strength', 6, 3, 1)
+#defender = Hero('Maximus', 650, 30, 'strength', 6, 3, 1)
+#prophet = Hero('Furion', 350, 25, 'intelligence', 2, 4, 7)
+
+create_5_monsters_no_prop = (np.random.choice(characters, 5).tolist())
+print(create_5_monsters_no_prop, end='\n'+'*' * 30+'\n')
+
+create_25_monsters = [Monster_creater(characters, properties).generate_monster() for i in range(25)]
+print(create_25_monsters)
