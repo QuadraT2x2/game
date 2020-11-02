19c80e82ddfb6355082bd3b6aa96c34784095c6f
Kotov Sergey <serjok170394@yandex.ru>
2020-11-02 16:03:46 +0300

    отдельный файл screen_confg.py

--- erunda.py
+++ erunda.py
@@ -1,19 +1,26 @@
 import pygame
 import sys
-#изменения
+
+from screen_config import margin, width, height, columns, rows
+
 pygame.init()
-width = height = 110
-margin = 25
-columns = rows = 6
+
+
 screenx = (width * columns) + (margin * (columns + 1))
 screeny = (height * rows) + margin * (rows + 1)
+
 screen = pygame.display.set_mode((screenx, screeny))
-pygame.display.set_caption('Erunda')
+
+white_square = pygame.image.load("square_white.png")
+white_square = pygame.transform.scale(white_square, (width, height))
 bricks = pygame.image.load("bricks.jpg")
 background = pygame.transform.scale(bricks, (screenx, screeny))
 img = pygame.image.load("ayaya.jpg")
-image = pygame.transform.scale(img, (width, height))
+ayaya_image = pygame.transform.scale(img, (width, height))
+
 pygame.display.set_icon(img)
+pygame.display.set_caption('Erunda')
+
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
@@ -31,6 +38,13 @@ def click_on_screen():
 
 mas = [[0] * columns for i in range(rows)]
 
+
+def draw_image_in_square(square_row, square_column, image):
+    square_x = margin + (margin + width) * square_column
+    square_y = margin + (margin + height) * square_row
+    screen.blit(image, (square_x, square_y))
+
+
 while True:
     # добавить строчку, которая будет рисовать картинку.
     screen.blit(background, (0, 0))
@@ -46,19 +60,15 @@ while True:
     for row in range(rows):
         for column in range(columns):
             # передавали в функцию, рисующую в ряде или колонке текущий ряд, 
-            # текущий столб, 
-
-            x = column * width + (column + 1) * margin
-            y = row * height + (row + 1) * margin
+            # текущий столб,
             # тут только вызываем draw_image_in_square
             if mas[row][column] == 1:
-                screen.blit(image, (x, y))
+                draw_image_in_square(row, column, ayaya_image)
             else:
                 # вместо белого прямоугольника рисуем белую картинку
-                pygame.draw.rect(screen, white, (x, y, width, height))
+                draw_image_in_square(row, column, white_square)
 
 
     # обновляет картинку
     pygame.display.update()
 
-def draw_image_in_square(square_row, square_column, image):
---
+++ screen_config.py
@@ -0,0 +1,3 @@
+width = height = 110
+columns = rows = 6
+margin = 25
