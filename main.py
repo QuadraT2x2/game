d1ecc356ca2f725004d3b9c9e08fe17df8f57834
Kotov Sergey <serjok170394@yandex.ru>
2020-11-09 18:32:45 +0300

    erunda.py => main.py + issue #5

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 from graphics_utils import draw_image_in_square
 from images import kitten, dog, chiken, dragon, tiger, ayaya, background, white_square
-from screen_config import margin, width, height, columns, rows, screen
+from screen_config import margin, width, height, columns, rows, screen, screenx
 
 pygame.init()
 
@@ -13,18 +13,23 @@ pygame.display.set_icon(ayaya)
 pygame.display.set_caption('Erunda')
 
 black = (0, 0, 0)
+white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
-white = (255, 255, 255)
+blue = (0, 0, 255)
+aqua = (0, 255, 255)
+purple = (255, 0, 255)
+yellow = (255, 255, 0)
 
 
 def click_on_screen():
     x_mouse, y_mouse = pygame.mouse.get_pos()
     print(f'x = {x_mouse} y = {y_mouse}')
-    column = min((columns - 1), x_mouse // (margin + width))
-    row = min((rows - 1), y_mouse // (margin + height))
-    print(f'row = {row} column = {column}')
-    mas[row][column] ^= 1
+    if x_mouse <= screenx:
+        column = min((columns - 1), x_mouse // (margin + width))
+        row = min((rows - 1), y_mouse // (margin + height))
+        print(f'row = {row} column = {column}')
+        mas[row][column] ^= 1
 
 
 mas = [[0] * columns for i in range(rows)]
@@ -32,6 +37,10 @@ mas = [[0] * columns for i in range(rows)]
 while True:
     # добавить строчку, которая будет рисовать картинку.
     screen.blit(background, (0, 0))
+    # добавить на новую зону в экране текстовое поле с текстом "hello, text!"
+    font = pygame.font.Font(None, 75)  # це шрифт
+    text = font.render("Hello, text!", True, purple)
+    screen.blit(text, [screenx + margin, margin])
     # обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -61,4 +70,3 @@ while True:
 
     # обновляет картинку
     pygame.display.update()
-
--- screen_config.py
+++ screen_config.py
@@ -5,6 +5,7 @@ columns = rows = 6
 margin = 25
 
 screenx = (width * columns) + (margin * (columns + 1))
+screenx_new = screenx + 360
 screeny = (height * rows) + margin * (rows + 1)
 
-screen = pygame.display.set_mode((screenx, screeny))
+screen = pygame.display.set_mode((screenx_new, screeny))
